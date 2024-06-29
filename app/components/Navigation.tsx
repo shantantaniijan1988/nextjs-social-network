7f66a4b3f41db659a74390663c576a3f6546310a
shantan <xojaniijan1988@gmail.com>
2024-06-29 21:14:06 +0900

    drawerの追加

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Drawer } from "@/app/components/Drawer";
 import { auth, signOut } from "@/auth";
 import { type FC } from "react";
 import Link from "next/link";
@@ -5,6 +6,8 @@ import Link from "next/link";
 export const Navigation: FC = async () => {
   const session = await auth();
   const user = session?.user;
+
+  const email = user?.email;
 
   const logoutAction = async () => {
     "use server";
@@ -21,16 +24,15 @@ export const Navigation: FC = async () => {
         </nav>
         {user && (
           <div className="ml-auto flex items-center gap-x-8">
-            <Link href="/dashboard" className="py-1 hover:underline">
-              ダッシュボード
-            </Link>
-            <form action={logoutAction}>
-              <div>
-                <button type="submit" className="py-1 hover:underline">
-                  ログアウト
-                </button>
-              </div>
-            </form>
+            <Drawer email={email!}>
+              <form action={logoutAction}>
+                <div>
+                  <button type="submit" className="py-1 hover:underline">
+                    ログアウト
+                  </button>
+                </div>
+              </form>
+            </Drawer>
           </div>
         )}
       </div>

3428f6969c31a85423785a7c4d8e2a665dc7146f
shantan <xojaniijan1988@gmail.com>
2024-06-30 20:36:33 +0900

    middleware.tsのmatcherの指定

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
 export { auth as middleware } from "@/auth";
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|lib|components).*)"],
+// };
+
+export const config = {
+  matcher: ["/", "/register", "/dashboard"],
+};
